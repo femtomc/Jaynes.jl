129b9b5904dbf4824d4da7346399f05888c1361a
McCoy R. Becker <mccoybecker@gmail.com>
2020-03-12 17:11:41 -0400

    Update - finished a simple log PDF transform which takes an IR form of a probabilistic program to a function which computes the log PDF when given a trace. Only supports univariate normal - must expand.

--- scratch/logpdf_dynamo.jl
+++ scratch/logpdf_dynamo.jl
@@ -2,41 +2,36 @@ module LogPDFTransform
 
 using Jaynes
 
-using Flux
-using Flux: Recur, RNNCell
-using CuArrays
-
-using IRTools: func
-
 using IRTools
+using IRTools: func
 
 using Distributions
 using DistributionsAD: logpdf
+
+using Zygote: gradient
 
 # Stochastic kung-foo!
 function foo(z::Float64)
     x = rand(Normal(z, 1.0))
     y = rand(Normal(x, 1.0))
-    return y
-end
-
-# Transformed.
-function logpdf_foo(z, x, y)
-    x_logpdf = logpdf(Normal(z, 1), x)
-    y_logpdf = logpdf(Normal(x, 1), y)
-    return x_logpdf + y_logpdf
+    l = rand(Normal(x + y, 3.0))
+    return l
 end
 
 ir = @code_ir foo(5.0)
-println(ir)
+println("\nOriginal:\n", ir, "\n")
 
 # Testing.
-logprob = func(@code_ir logpdf_transform! foo(5.0))
+transformed = @code_ir logpdf_transform! foo(5.0)
+println("Transformed:\n", transformed, "\n")
+logprob = func(transformed)
 
-println(logprob(0.3, 3.0, 5.0))
-
+println(logprob(0.3, 3.0, 5.0, 5.0))
 for i in [1.0, 2.0, 5.0]
-    println(logprob(i, -i, i))
+    println(logprob(i, -i, i, 5.0))
 end
+
+grad = gradient((x, y, z, k) -> logprob(x, y, z, k), 0.3, 3.0, 5.0, 5.0)
+println("\nGradient:\n", grad)
 
 end #module
