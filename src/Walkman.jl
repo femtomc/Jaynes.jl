5d3997b62cd2bc355f34cdb21fb242cefaad0357
McCoy R. Becker <mccoybecker@gmail.com>
2020-05-23 12:58:13 -0400

    Fixed a bug in recording the score.

--- src/Walkman.jl
+++ src/Walkman.jl
@@ -7,7 +7,7 @@ using Cassette: recurse, similarcontext, disablehooks
 using Distributions
 using ExportAll
 
-include("core.jl")
+include("trace_core.jl")
 include("trace_context.jl")
 include("utils.jl")
 include("importance_sampling.jl")
