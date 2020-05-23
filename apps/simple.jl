628b44a97cb3e811e2cd89eb69da25d1077be4d4
McCoy R. Becker <mccoybecker@gmail.com>
2020-05-23 18:51:57 -0400

    Playing around with ideas for differentiability...

--- apps/simple.jl
+++ apps/simple.jl
@@ -4,15 +4,14 @@ include("../src/Walkman.jl")
 using .Walkman
 using Distributions
 
-function foo1()
-    z = 10.0
+function foo1(z)
     x = rand(:x, Normal, (z, 1.0))
     y = rand(:y, Normal, (x, 1.0))
     return y
 end
 
 obs = constraints([(:y, 5.0)])
-tr = trace(foo1, (), obs)
+tr = trace(foo1, (0.0,), obs)
 println(tr, [:val])
 
 end # module
--- scratch/gradient_core.jl
+++ scratch/gradient_core.jl
@@ -1,33 +1,36 @@
 module GradientCore
 
 using Cassette
-using Cassette: recurse, Reflection
-using IRTools
-using MacroTools
-using InteractiveUtils
-using Core: CodeInfo
+using Cassette: recurse, Reflection, disablehooks, disablehooks, disablehooks, disablehooks
+using Distributions
+using DistributionsAD
+using Zygote
 
 Cassette.@context GradientContext
 
-Cassette.prehook(::GradientContext, f, args...) = println(f, args)
-
-# This pass requires that the context has access to a map from param names to gradients.
-function identity(ctx::Type{<:GradientContext}, ir::Reflection) where {S}
-    println(ir)
-    code_info = ir.code_info
-    println(code_info)
-    return code_info
+struct Accumulator
+    tape::Dict{Symbol, Tuple}
 end
 
-const identity_pass = Cassette.@pass identity
+function Cassette.overdub(::GradientContext, f::typeof(rand), addr::Symbol, dist::Type, args)
+    sample = f(dist(args...))
+    ∇ = gradient((x, args) -> logpdf(dist(args...), x), sample, args)
+    println(∇)
+    ctx.metadata.tape[addr] = ∇
+    return sample
+end
+    
 
-function foo(y)
-    x = 6 * y
-    x += 10
-    return x
+function foo1(z)
+    x = rand(:x, Normal, (z, 1.0))
+    y = rand(:y, Normal, (x, 1.0))
+    return y
 end
 
-ctx = GradientContext(pass = identity_pass, metadata = "Hi!")
-println(Cassette.overdub(ctx, foo, 5))
+ctx = disablehooks(GradientContext(metadata = Accumulator(Dict{Symbol, Tuple}())))
+
+out = Cassette.overdub(ctx, foo1, 5.0)
+println(out)
+println(ctx.metadata)
 
 end # module
