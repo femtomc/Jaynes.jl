608363b13dac5c4249fefa1db5b9d78a4d48d8a9
McCoy R. Becker <mccoybecker@gmail.com>
2020-05-24 20:53:23 -0400

    Optimized a number of little things.

--- examples/time_series.jl
+++ examples/time_series.jl
@@ -4,13 +4,13 @@ include("../src/Walkman.jl")
 using .Walkman
 using Distributions
 
-transition(z::Float64, addr) = rand(addr, Normal(0.0, 1.0))
-observation(z::Float64, addr) = rand(addr, Normal(0.0, 1.0))
+transition(z::Float64, addr) = rand(addr, Normal, (0.0, 1.0))
+observation(z::Float64, addr) = rand(addr, Normal, (0.0, 1.0))
 
 function simulate(init_z_params::Tuple{Float64, Float64}, T::Int)
     μ, σ = init_z_params
     observations = Vector{Float64}(undef, T)
-    z = rand(:z₀, Normal(μ, σ))
+    z = rand(:z₀, Normal,(μ, σ))
     observations[1] = observation(z, :x₀)
     for i in 2:T
         z = transition(z, :z => i)
@@ -19,12 +19,7 @@ function simulate(init_z_params::Tuple{Float64, Float64}, T::Int)
     return observations
 end
 
-tr = Trace()
-obs = tr() do
-    simulate((0.5, 3.0), 50)
-end
-
-println(tr, [:val])
-println(obs)
+tr, weight = trace(simulate, ((0.5, 5.0), 50))
+display(tr)
 
 end # module
