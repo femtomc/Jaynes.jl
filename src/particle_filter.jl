9d614c429611014b328e2dcb07752965aec86307
McCoy R. Becker <mccoybecker@gmail.com>
2020-05-26 17:33:18 -0400

    Particle filter implementation closely follows importance sampling. Instead of Generate, we instead use Update.

---
+++ src/particle_filter.jl
@@ -0,0 +1,91 @@
+function filter_step!(ctx::TraceCtx{M},
+                      trs::Vector{Trace}, 
+                      args::Tuple,
+                      observations::Dict{Address, T}) where {T, M <: UnconstrainedGenerateMeta}
+    num_p = length(trs)
+    lws = Vector{Float64}(undef, num_p)
+    rets = Vector{Any}(undef, num_p)
+    update_ctx = Update(Trace(), observations)
+
+    for i in 1:num_p
+        # Run update.
+        update_ctx.metadata.tr =  trs[i]
+        if !isempty(args)
+            ret = Cassette.overdub(update_ctx, ctx.func, args...)
+        else
+
+            ret = Cassette.overdub(update_ctx, ctx.func)
+        end
+
+        # Store.
+        trs[i] = new_tr
+        lws[i] = new_score
+        rets[i] = update_ctx.metadata.ret
+    end
+
+    update_ctx.metadata.ret = rets
+    update.ctx.metadata.func = ctx.func
+    update_ctx.metadata.args = args
+    return update_ctx, trs, lws, rets
+end
+
+function filter_step!(ctx::TraceCtx{M},
+                      trs::Vector{Trace}, 
+                      args::Tuple,
+                      proposal::Function,
+                      proposal_args::Tuple,
+                      observations::Dict{Address, T}) where {T, M <: UnconstrainedGenerateMeta}
+    num_p = length(trs)
+    lws = Vector{Float64}(undef, num_p)
+    rets = Vector{Any}(undef, num_p)
+    prop_ctx = Proposal(Trace())
+    update_ctx = Update(Trace(), constraints)
+
+    for i in 1:num_p
+        # Propose.
+        if isempty(proposal_args)
+            Cassette.overdub(prop_ctx, proposal)
+        else
+            Cassette.overdub(prop_ctx, proposal, proposal_args...)
+        end
+
+        # Merge proposals and observations.
+        prop_score = prop_ctx.metadata.tr.score
+        prop_chm = prop_ctx.metadata.tr.chm
+        constraints = merge(observations, prop_chm)
+
+        # Run update.
+        update_ctx.metadata.tr =  trs[i]
+        update_ctx.metadata.constraints = constraints
+        if isempty(args)
+            ret = Cassette.overdub(update_ctx, ctx.func)
+        else
+            ret = Cassette.overdub(update_ctx, ctx.func args...)
+        end
+
+        # Store.
+        trs[i] = update_ctx.metadata.tr
+        lws[i] = update_ctx.metadata.score - pop_score
+        rets[i] = ret
+    end
+
+    update_ctx.metadata.ret = rets
+    update_ctx.metadata.func = ctx.func
+    update_ctx.metadata.args = args
+    return ctx, trs, lws, rets
+end
+
+function resample!(trs::Vector{Trace}, lws::Vector{Float64})
+    num_p = length(trs)
+    ltw, lnw = nw(lws)
+    weights = exp.(lnw)
+    selections = rand(Categorical(weights/sum(weights)), 1:num_p)
+    lmle += ltw - log(num_p)
+    trs = map(selections) do ind
+        trs[ind]
+    end
+    lws = map(lws) do k
+        0.0
+    end
+    return trs, lws, lmle
+end
--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,4 @@
+# Log sum exp.
 function lse(arr)
     max = maximum(arr)
     max == -Inf ? -Inf : max + log(sum(exp.(arr .- max)))
@@ -6,6 +7,19 @@ end
 function lse(x1::Real, x2::Real)
     m = max(x1, x2)
     m == -Inf ? m : m + log(exp(x1 - m) + exp(x2 - m))
+end
+
+# Effective sample size.
+function ess(lnw::Vector{Float64})
+    log_ess = -lse(2. * lnw)
+    return exp(log_ess)
+end
+
+# Normalize log weights.
+function nw(lw::Vector{Float64})
+    lt = logsumexp(lw)
+    lnw = lw .- lt
+    return (lt, lnw)
 end
 
 # This is likely not a well-defined expectation. Investigate.
