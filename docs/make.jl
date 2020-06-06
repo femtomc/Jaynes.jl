6026cd47c2d91be5c96053ad832ae2deae5d38b8
McCoy R. Becker <mccoybecker@gmail.com>
2020-06-06 06:58:08 -0700

    Setting up documentation and separate example dir.

--- docs/make.jl
+++ docs/make.jl
@@ -2,15 +2,16 @@ using Documenter
 
 makedocs(sitename = "Jaynes.jl",
          pages = [
-                  "Introduction" => "index.md",
-                  "Concepts" => "concepts.md",
+                  #"Introduction" => "index.md",
+                  #"Concepts" => "concepts.md",
                   "Implementation architecture" => "architecture.md",
-                  "Examples" => [
-                                 "Bayesian linear regression" => "bayeslinreg.md",
-                                 "Autoencoding with black box extensions" => "autoencoding.md",
-                                 "Gaussian process kernel synthesis" => "gp_kernel_synth.md",
-                                 "Inference compilation" => "infcomp.md"],
-                  "Related work" => "related_work.md"],
+                  #"Examples" => [
+                  #               "Bayesian linear regression" => "bayeslinreg.md",
+                  #               "Autoencoding with black box extensions" => "autoencoding.md",
+                  #               "Gaussian process kernel synthesis" => "gp_kernel_synth.md",
+                  #               "Inference compilation" => "infcomp.md"],
+                  #"Related work" => "related_work.md"
+                  ],
          format = Documenter.HTML(prettyurls = false,
                                   assets = ["assets/favicon.ico"])
         )
