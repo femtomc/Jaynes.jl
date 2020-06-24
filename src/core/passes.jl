973ed8c0cee74a97d2f18cf7a68f4175e5e81f2b
McCoy R. Becker <mccoybecker@gmail.com>
2020-06-23 23:12:54 -0700

    Refactored ignore pass - it's still causing bugs, which I have no idea why.

--- src/core/passes.jl
+++ src/core/passes.jl
@@ -21,24 +21,13 @@ end
 # This is a pass which indicates to native Cassette passes that the only thing of interest is calls to rand.
 function ignore_transform(::Type{<:TraceCtx}, r::Reflection)
     syn = r.code_info.code
-    map(syn) do expr
-        MacroTools.postwalk(expr) do k
-            # If you already wrapped, don't wrap.
-            k isa Expr && k.head == :call && begin
-                arg = k.args[1]
-                arg isa Expr && arg.head == :nooverdub && return k
+    new_code = Expr[]
+    for k in syn
+        if k isa Expr && k.head == :call
+            arg = k.args[1]
+            if !(arg isa Expr && arg.head == :nooverdub) && !(arg isa GlobalRef && arg.name == :rand)
+                k.args[1] = Expr(:nooverdub, arg)
             end
-
-            # If you haven't wrapped, wrap.
-            k isa Expr && k.head == :call && begin
-                call = k.args[1]
-                if !(call isa GlobalRef && call.name == :rand)
-                    k.args[1] = Expr(:nooverdub, call)
-                    return k
-                end
-            end
-            
-            k
         end
     end
     return r.code_info
