e99e8c67261b65ea0a086e7359591140da3f7917
McCoy R. Becker <mccoybecker@gmail.com>
2020-07-01 12:34:11 -0700

    Back to original Jaynes functionality.

--- src/Jaynes.jl
+++ src/Jaynes.jl
@@ -1,89 +1,39 @@
 module Jaynes
 
-using Cthulhu
-
-# IRRRR I'm a com-pirate.
-using Cassette
-using Cassette: recurse, similarcontext, disablehooks, Reflection, canrecurse
-import Cassette: overdub, prehook, posthook, Reflection, fallback
-using MacroTools
-using MacroTools: postwalk
 using IRTools
-using IRTools: meta, IR, slots!, Variable, typed_meta
-import IRTools: meta, IR
-using Mjolnir
-using Logging
-using Dates
-
+using IRTools: @dynamo, IR, xcall, arguments, insertafter!, recurse!, isexpr, self, argument!
 using Distributions
 
-# Differentiable goop.
-using Zygote
-using DistributionsAD
-
+# Toplevel importants :)
+abstract type ExecutionContext end
 const Address = Union{Symbol, Pair{Symbol, Int64}}
 
-include("core/static.jl")
-include("core/trace.jl")
-include("core/selections.jl")
-include("core/contexts.jl")
-include("core/blackbox.jl")
-include("core/language_cores.jl")
+include("trace.jl")
+include("selections.jl")
+include("contexts.jl")
 include("utils.jl")
 include("inference/importance_sampling.jl")
-include("inference/particle_filter.jl")
+include("inference/particle_filtering.jl")
 include("inference/metropolis_hastings.jl")
-include("tracing.jl")
-include("core/passes.jl")
 
-# Allows debug tracing in packages which use Jaynes.
-function derive_debug(mod; path = "jayneslog_$(Time(Dates.now())).txt", type_tracing = false, all_calls = false)
-    io = open(path, "w+")
-    logger = Logging.SimpleLogger(io)
-    Logging.global_logger(logger)
-    @assert mod isa Module
-    fns = filter(names(mod)) do nm
-        try
-            Base.eval(mod, nm) isa Function
-        catch e
-            println("Ignoring call in $e.")
-            false
-        end
-    end
-    if type_tracing
-        @eval begin
-            using Revise
-        end
-    end
-
-    @info "Jaynes says: deriving debug calls.\n$(map(x -> String(x) * "\n", fns)...)"
-    exprs = map(fns) do f
-        if type_tracing
-            @eval mod begin
-                function Jaynes.prehook(::Jaynes.TraceCtx, call::typeof($mod.$f), args...)
-                    @info "\n$(stacktrace()[3])\n" call typeof(args)
-                    println("Beginning type inference...")
-                    Cthulhu.descend(call, typeof(args))
-                end
-            end
-        else
-            @eval mod begin
-                function Jaynes.prehook(::Jaynes.TraceCtx, call::typeof($mod.$f), args...)
-                    @info "\n$(stacktrace()[3])\n" call typeof(args)
-                end
-            end
-        end
-    end
-
-    if all_calls
-        @eval mod begin
-            function Jaynes.prehook(::Jaynes.TraceCtx, call::Function, args...)
-                @info "\n$(stacktrace()[3])\n" call typeof(args)
-            end
-        end
-    end
-    
-    return logger 
+function call(tr::Trace, fn::Function, args...)
+    ret = tr(fn, args...)
+    return CallSite(tr, fn, args, ret)
 end
+
+# Contexts.
+export Generate, Update, Propose, Regenerate, Score
+
+# Trace.
+export Trace
+
+# Selections.
+export selection, compare
+
+# Inference.
+export importance_sampling, initialize_filter, filter_step!, metropolis_hastings
+
+# Utilities.
+export display, merge
 
 end # module
