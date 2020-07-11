f7405cd78155f52720611bea55606af94237c847
McCoy R. Becker <mccoybecker@gmail.com>
2020-07-10 22:04:27 -0700

    Including docstrings, etc.

--- build_docs.sh
+++ build_docs.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 julia --project=docs/ -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.instantiate()'
 julia --project=docs/ docs/make.jl
--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 julia --project=. -e 'using Pkg; Pkg.add("Test"); Pkg.instantiate(); Pkg.test(); Pkg.rm("Test")'
