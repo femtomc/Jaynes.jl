f9b8702ac95cd1c807393b6ad52722cbdbfab6c4
McCoy R. Becker <mccoybecker@gmail.com>
2020-07-13 08:49:49 -0700

    Added conversion to/from arrays for utility selections (like gradients and learnable parameters) - will allow hook into Flux optimiser APIs.

--- src/selections.jl
+++ src/selections.jl
@@ -5,11 +5,90 @@ import Base: +
 
 # Abstract type for a sort of query language for addresses within a particular method body.
 abstract type Selection end
+
+# ------------ Generic conversion to and from arrays ------------ #
+
+# Subtypes of Selection should implement their own fill_array! and from_array.
+# Right now, these methods are really only used for gradient-based learning.
+
+function array(gs::Selection, ::Type{T}) where T
+    arr = Vector{T}(undef, 32)
+    n = fill_array!(gs, arr, 1)
+    resize!(arr, n)
+    arr
+end
+
+function fill_array!(val::T, arr::Vector{T}, f_ind::Int) where T
+    if length(arr) < f_ind
+        resize!(arr, 2 * f_ind)
+    end
+    arr[f_ind] = val
+    1
+end
+
+function fill_array!(val::Vector{T}, arr::Vector{T}, f_ind::Int) where T
+    if length(arr) < f_ind + length(val)
+        resize!(arr, 2 * (f_ind + length(val)))
+    end
+    arr[f_ind : f_ind + length(val) - 1] = val
+    length(val)
+end
+
+function selection(schema::Selection, arr::Vector)
+    (n, sel) = from_array(schema, arr, 1)
+    n != length(arr) && error("Dimension error: length of arr $(length(arr)) must match $n.")
+    sel
+end
+
+function from_array(::T, arr::Vector{T}, f_ind::Int) where T
+    (1, arr[f_ind])
+end
+
+function from_array(val::Vector{T}, arr::Vector{T}, f_ind::Int) where T
+    n = length(val)
+    (n, arr[f_ind : f_ind + n - 1])
+end
+
 abstract type UtilitySelection <: Selection end
+
+function fill_array!(val::T, arr::Vector{K}, f_ind::Int) where {K, T <: UtilitySelection}
+    sorted_toplevel_keys = sort(collect(keys(val.utility)))
+    sorted_tree_keys  = sort(collect(keys(val.tree)))
+    idx = f_ind
+    for k in sorted_toplevel_keys
+        v = val.utility[k]
+        n = fill_array!(v, arr, idx)
+        idx += n
+    end
+    for k in sorted_tree_keys
+        n = fill_array!(get_sub(val, k), arr, idx)
+        idx += n
+    end
+    idx - f_ind
+end
+
+function from_array(schema::T, arr::Vector{K}, f_ind::Int) where {K, T <: UtilitySelection}
+    # Subtypes of UtilitySelection must support an empty constructor.
+    sel = T()
+    sorted_toplevel_keys = sort(collect(keys(schema.utility)))
+    sorted_tree_keys  = sort(collect(keys(schema.tree)))
+    idx = f_ind
+    for k in sorted_toplevel_keys
+        (n, v) = from_array(schema.utility[k], arr, idx)
+        idx += n
+        sel.utility[k] = v
+    end
+    for k in sorted_tree_keys
+        (n, v) = from_array(get_sub(schema, k), arr, idx)
+        idx += n
+        sel.tree[k] = v
+    end
+    (idx - f_ind, sel)
+end
 
 # ------------ Lightweight visitor ------------ #
 
-struct Visitor <: UtilitySelection
+struct Visitor <: Selection
     tree::Dict{Address, Visitor}
     addrs::Vector{Address}
     Visitor() = new(Dict{Address, Visitor}(), Address[])
