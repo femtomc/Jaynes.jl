b118399b6b052b04d10dd427d62f536230c83a55
McCoy R. Becker <mccoybecker@gmail.com>
2020-08-01 16:20:13 -0700

    Fixed README.md example, and bayeslinreg.jl example.

--- examples/bayeslinreg.jl
+++ examples/bayeslinreg.jl
@@ -2,34 +2,33 @@ module BayesianLinearRegression
 
 include("../src/Jaynes.jl")
 using .Jaynes
-using Distributions
 
-function bayesian_linear_regression(N::Int)
+function bayesian_linear_regression(x::Vector{Float64})
     σ = rand(:σ, InverseGamma(2, 3))
     β = rand(:β, Normal(0.0, 1.0))
-    y = plate(:y, x -> rand(:draw, Normal(β*x, σ)), [Float64(i) for i in 1 : N])
+    y = Vector{Float64}(undef, length(x))
+    for i in 1 : length(x)
+        push!(y, rand(:y => i, Normal(β * x[i], σ)))
+    end
+    return y
 end
 
-# Example trace.
-ret, cl = simulate(bayesian_linear_regression, 10)
-display(cl.trace)
-
-sel = selection(map(1 : 100) do i
-                    (:y => i => :draw, Float64(i) + rand())
+x = [Float64(i) for i in 1 : 100]
+obs = selection(map(1 : 100) do i
+                    (:y => i, ) => 3.0 * x[i] + rand()
                 end)
 
-@time ps, ret = importance_sampling(sel, 5000, bayesian_linear_regression, (100, ))
+n_samples = 5000
+@time ps, ret = importance_sampling(obs, n_samples, bayesian_linear_regression, (x, ))
 
-num_res = 1000
-resample!(ps, num_res)
 mean_σ = sum(map(ps.calls) do cl
-                 cl[:σ]
-             end) / num_res
+                 get_ret(cl[:σ])
+             end) / n_samples
 println("Mean σ: $mean_σ")
 
 mean_β = sum(map(ps.calls) do cl
-                 cl[:β]
-             end) / num_res
+                 get_ret(cl[:β])
+             end) / n_samples
 println("Mean β: $mean_β")
 
 end # module
