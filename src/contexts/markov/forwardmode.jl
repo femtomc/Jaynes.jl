464620df620014cc0ee155eaa8e4adff1d6d6254
McCoy R. Becker <mccoybecker@gmail.com>
2020-08-28 15:52:13 -0400

    Refactor specialized call sites.

--- src/contexts/markov/forwardmode.jl
+++ src/contexts/markov/forwardmode.jl
@@ -1,30 +1,24 @@
-# ------------ Choice sites ------------ #
-
-@inline function (ctx::ForwardModeContext)(c::typeof(plate),
-                                           addr::A,
+@inline function (ctx::ForwardModeContext)(c::typeof(markov),
                                            d::Distribution{K},
                                            len::Int) where A <: Address
     v_ret = Vector{eltype(d)}(undef, len)
     for i in 1:len
         visit!(ctx, i)
-        s = context_getindex(ctx, ctx.map, addr => i)
+        s = context_getindex(ctx, ctx.map, i)
         ctx.weight += logpdf(d, s)
         v_ret[i] = s
     end
     return v_ret
 end
 
-# ------------ Call sites ------------ #
-
-@inline function (ctx::ForwardModeContext)(c::typeof(plate),
-                                           addr::A,
+@inline function (ctx::ForwardModeContext)(c::typeof(markov),
                                            call::Function,
-                                           args...) where A <: Address
+                                           args...)
     # First index.
-    visit!(ctx, addr => 1)
+    visit!(ctx, 1)
     len = length(args)
-    ss = get_sub(ctx.target, (addr, 1))
-    ps = get_sub(ctx.params, (addr, 1))
+    ss = get_sub(ctx.target, 1)
+    ps = get_sub(ctx.params, 1)
     ret, cl, w = forward(ctx.target[2 : end], ps, ss, Dual(1.0, 0.0))
     v_ret = Vector{typeof(ret)}(undef, len)
     v_ret[1] = ret
@@ -32,11 +26,23 @@ end
 
     # Rest.
     for i in 2:len
-        visit!(ctx, addr => i)
-        ss = get_sub(ctx.target, (addr, i))
+        visit!(ctx, i)
+        ss = get_sub(ctx.target, i)
         ret, cl, w = forward(ctx.target[2 : end], ps, ss, Dual(1.0, 0.0))
         v_ret[i] = ret
         ctx.weight += w
     end
     return v_ret
+end
+
+@inline function (ctx::ForwardModeContext)(c::typeof(markov),
+                                           addr::A,
+                                           call::Function,
+                                           args...) where A <: Address
+    visit!(ctx, addr)
+    ps = get_sub(ctx.params, addr)
+    ss = get_sub(ctx.map, addr)
+    ret, w = forward(ctx.target[2 : end], ps, ss, Dual(1.0, 0.0))
+    ctx.weight += w
+    return ret
 end
--- src/contexts/plate/forwardmode.jl
+++ src/contexts/plate/forwardmode.jl
@@ -1,11 +1,10 @@
 @inline function (ctx::ForwardModeContext)(c::typeof(plate),
-                                           addr::A,
                                            d::Distribution{K},
                                            len::Int) where A <: Address
     v_ret = Vector{eltype(d)}(undef, len)
     for i in 1:len
         visit!(ctx, i)
-        s = context_getindex(ctx, ctx.map, addr => i)
+        s = context_getindex(ctx, ctx.map, i)
         ctx.weight += logpdf(d, s)
         v_ret[i] = s
     end
@@ -17,10 +16,10 @@ end
                                            call::Function,
                                            args...)
     # First index.
-    visit!(ctx, addr => 1)
+    visit!(ctx, 1)
     len = length(args)
-    ss = get_sub(ctx.target, (addr, 1))
-    ps = get_sub(ctx.params, (addr, 1))
+    ss = get_sub(ctx.target, 1)
+    ps = get_sub(ctx.params, 1)
     ret, cl, w = forward(ctx.target[2 : end], ps, ss, Dual(1.0, 0.0))
     v_ret = Vector{typeof(ret)}(undef, len)
     v_ret[1] = ret
@@ -28,8 +27,8 @@ end
 
     # Rest.
     for i in 2:len
-        visit!(ctx, addr => i)
-        ss = get_sub(ctx.target, (addr, i))
+        visit!(ctx, i)
+        ss = get_sub(ctx.target, i)
         ret, cl, w = forward(ctx.target[2 : end], ps, ss, Dual(1.0, 0.0))
         v_ret[i] = ret
         ctx.weight += w
