fa1e34f1258d85676ad77c567b2e7525350ff97c
femtomc <mccoybecker@gmail.com>
2020-11-09 11:14:40 -0500

    More descriptive comment in specializer.

--- src/typing_rules.jl
+++ src/typing_rules.jl
@@ -1,13 +1,14 @@
 # ------------ Trace typing rules ------------ #
 
-
-# Rules.
+# Distributions.
 @abstract TracePrimitives (::D)(args...) where D <: Distributions.Distribution =  D
 @abstract TracePrimitives (::Type{D})(args...) where D <: Distributions.Distribution = D
+
+# Calls to trace.
 @abstract TracePrimitives trace(::Symbol, ::Normal) = Reals
 @abstract TracePrimitives trace(::Symbol, ::Bernoulli) = Discrete{2}
 @abstract TracePrimitives trace(::Symbol, jfn::JFunction{N, R, T}, args...) where {N, R, T} = get_trace_type(jfn.value)
-@abstract TracePrimitives function trace(::Symbol, u::Gen.Unfold, args...) where {N, R, T}
-    val = u.value
-    List{get_trace_type(val.kernel)}
-end
+
+# Combinators.
+@abstract TracePrimitives trace(::Symbol, u::Gen.Unfold, args...) = List{get_trace_type(u.value.kernel)}
+@abstract TracePrimitives trace(::Symbol, u::Gen.Map, args...) = List{get_trace_type(u.value.kernel)}
